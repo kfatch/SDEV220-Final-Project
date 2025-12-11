1544895373160917fc356f08b41a0d8b7b19f5a8
Kody <kfatch@ivytech.edu>
2025-12-11 14:33:51 -0500

    menu nav complete, started work on functions

--- final_project.py
+++ final_project.py
@@ -42,22 +42,53 @@ def animal_menu():
     print("Show Animal List: 3")
     print("Go Back:          4")
     print("Quit:             0")
+def animal_add():
+    return print("animal add option")
+def animal_delete():
+    return print("animal delete option")
+def animal_list():
+    return print("animal list option")
 def inventory_menu(): 
     print("Add Item to Order List:         1")
     print("Remove Item from Order List:    2")
     print("Show Inventory List:            3")
     print("Go Back:                        4")
     print("Quit:                           0")
+def inventory_add():
+    return print("inventory add option")
+def inventory_remove():
+    return print("inventory remove option")
+def inventory_list():
+    return print("inventory list option")
 def revenue_menu():
     print("Add Profit:     1")
     print("Add Expense:    2")
-    print("Change a Value: 3")
+    print("Edit a Value:   3")
     print("Go Back:        4")
     print("Quit:           0")
+def revenue_profit():
+    return print("revenue profit option")
+def revenue_expense():
+    return print("revenue expense option")
+def revenue_edit():
+    return print("revenue edit option")
 
+#-------------------------Menu Navigation-------------------------------
 menu_lvl = 0    #Used for identify which menu the user is currently in. 0 for main(entry level), 1 for in a menu one level deep, etc
 menu_id = 0    #Used to identify which submenu the user is in. (0 for Main, 1 for Animal, 2 for Inventory, and 3 for Revenue)
 
+#--------------------------Animal--------------------------------------
+animal_list = []
+animal_dict = {}
+
+#-------------------------Inventory---------------------------------------
+#Inventory list = {item : quantity}
+inventory_dict = {}
+
+#--------------------------Revenue---------------------------------------
+
+
+#---------------------------------Start------------------------------------
 print("Welcome to Clay County Humane Society Management System.")
 print_menu()
 user_input = int(input("Select A Number To Proceed.\n"))
@@ -66,12 +97,14 @@ while (user_input < 0) or (user_input > 3):
 menu_lvl += 1
 while (user_input != 0):
     while (menu_lvl > -1) and (menu_lvl < 3):
+        #------------------------menu_lvl: 1 (selection menu for either Animal, Inventory, or Revenue)------------------------------
         while menu_lvl == 1:
             if user_input == 1:
                 animal_menu()
                 user_input = int(input("Select A Number To Proceed.\n"))
                 if (user_input > 0) and (user_input < 4):
                     menu_lvl += 1
+                    menu_id = 1
                 else:
                     menu_lvl -= 1
             elif user_input == 2:
@@ -79,6 +112,7 @@ while (user_input != 0):
                 user_input = int(input("Select A Number To Proceed.\n"))
                 if (user_input > 0) and (user_input < 4):
                     menu_lvl += 1
+                    menu_id = 2
                 else:
                     menu_lvl -= 1
             elif user_input == 3:
@@ -86,13 +120,49 @@ while (user_input != 0):
                 user_input = int(input("Select A Number To Proceed.\n"))
                 if (user_input > 0) and (user_input < 4):
                     menu_lvl += 1
+                    menu_id = 3
                 else:
                     menu_lvl -= 1
+        #-------------------------menu_lvl: 0 (start menu)---------------------------------
         while menu_lvl == 0:
             print_menu()
             user_input = int(input("Select A Number To Proceed.\n"))
             while (user_input < 0) or (user_input > 3):
                 user_input = int(input("Enter a valid option.\n"))
             menu_lvl += 1
+        #-------------------------menu_lvl: 2 (submenu of either Animal, Inventory, or Revenue after selection)-------------------------------------
         while menu_lvl == 2:
-            pass
+            #-------------------------Animal Submenu-----------------------------
+            if menu_id == 1:
+                if user_input == 1:
+                    animal_add()
+                    menu_lvl += 1
+                elif user_input == 2:
+                    animal_delete()
+                    menu_lvl += 1
+                elif user_input == 3:
+                    animal_list()
+                    menu_lvl += 1
+            #-------------------------Inventory Submenu--------------------------------  
+            elif menu_id == 2:
+                if user_input == 1:
+                    inventory_add()
+                    menu_lvl += 1
+                elif user_input == 2:
+                    inventory_remove()
+                    menu_lvl += 1
+                elif user_input == 3:
+                    inventory_list()
+                    menu_lvl += 1
+            #--------------------------Revenue Submenu--------------------------------
+            elif menu_id == 3:
+                if user_input == 1:
+                    revenue_profit()
+                    menu_lvl += 1
+                elif user_input == 2:
+                    revenue_expense()
+                    menu_lvl += 1
+                elif user_input == 3:
+                    revenue_edit()
+                    menu_lvl += 1
+        #--------------------------------menu_lvl: 3--------------------------------------------------------------------------------------
